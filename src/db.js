9eb3595f306af892103c04d454bedbbed96e2532
lostmart <martinpedraza1979@gmail.com>
2025-09-17 19:15:44 +0200

    new checkers

--- src/db.js
+++ src/db.js
@@ -62,7 +62,7 @@ export const initializeDatabase = () => {
           CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            email TEXT UNIQUE NOT NULL,
+            email TEXT NOT NULL,
             age INTEGER,
             gender TEXT CHECK(gender IN ('male', 'female')),
             avatar_url TEXT,
